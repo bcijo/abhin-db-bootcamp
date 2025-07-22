e51c6c094e4b69d69e4ffa1901cdd8ea4ef26ba3
bcijo <bbcijo@gmail.com>
2025-07-22 00:42:45 -0700

    Primitive datatypes

---
+++ db-demo-java/src/DataTypes.java
@@ -0,0 +1,8 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int var = 19;
+        String str = "rd july and pi stays ";
+        double pi = 3.14159;
+        System.out.println((var + 03) + " " + str + " " + pi);
+    }
+}
