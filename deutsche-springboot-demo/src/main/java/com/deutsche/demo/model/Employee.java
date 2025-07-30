372653f34a2efece404eeb2b2bdb2da27fe3ebd0
bcijo <bbcijo@gmail.com>
2025-07-30 12:15:58 +0530

    new changes

--- deutsche-springboot-demo/src/main/java/com/deutsche/demo/model/Employee.java
+++ deutsche-springboot-demo/src/main/java/com/deutsche/demo/model/Employee.java
@@ -1,16 +1,28 @@
 package com.deutsche.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Entity
 @Table(name = "employees")
 public class Employee {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK values
+    @Column(name = "id")
     private Integer id;
+
+    @NotNull(message = "name should be provided")
+    @NotBlank(message = "name can not be empty")
+    @Size(min = 2, max = 50, message = "Name should be between 2 and 50 characters.")
+    @Column(name = "name")
     private String name;
+
+    @Positive(message = "Salary should be more than 0.")
+    @Column(name = "salary")
     private Double salary;
 
     public Employee() {}
