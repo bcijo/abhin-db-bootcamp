0bb7383842da4fc0344da59dc541e42deb7f2d10
bcijo <bbcijo@gmail.com>
2025-07-30 13:32:47 +0530

    logging in service class

--- deutsche-springboot-demo/src/main/java/com/deutsche/demo/controller/EmployeeController.java
+++ deutsche-springboot-demo/src/main/java/com/deutsche/demo/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.deutsche.demo.controller;
 
 import com.deutsche.demo.model.Employee;
 import com.deutsche.demo.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,26 +15,32 @@ import java.util.List;
 @RequestMapping("employee")
 public class EmployeeController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private EmployeeService empService;
 
     @GetMapping
     public List<Employee> getAllEmployees() {
+        logger.info("Fetching all employees");
         return empService.getAllEmployees();
     }
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable(name = "id") Integer id) {
+        logger.debug("Fetching employee with ID: {}", id);
         return empService.getEmployeeById(id);
     }
 
     @PostMapping
     public Employee addEmployee(@Valid @RequestBody Employee employee) {
+        logger.info("Adding new employee: {}", employee.getName());
         return empService.addEmployee(employee);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable Integer id) {
+        logger.warn("Attempting to delete employee with ID: {}", id);
         empService.deleteEmployee(id);
         return ResponseEntity.ok("Employee with ID " + id + " deleted successfully.");
     }
--- deutsche-springboot-demo/src/main/java/com/deutsche/demo/service/EmployeeService.java
+++ deutsche-springboot-demo/src/main/java/com/deutsche/demo/service/EmployeeService.java
@@ -3,6 +3,8 @@ package com.deutsche.demo.service;
 import com.deutsche.demo.exception.EmployeeNotFoundException;
 import com.deutsche.demo.model.Employee;
 import com.deutsche.demo.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,23 +13,29 @@ import java.util.List;
 @Service
 public class EmployeeService {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     EmployeeRepository empRepository;
 
     public List<Employee> getAllEmployees() {
+        logger.info("Retrieving all employees from the database");
         return empRepository.findAll();
     }
 
     public Employee getEmployeeById(Integer id) {
+        logger.info("Adding new employee");
         return empRepository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
     }
 
     public Employee addEmployee(Employee employee) {
+        logger.info("Adding new employee: {}", employee.getName());
         return empRepository.save(employee);
     }
 
     public void deleteEmployee(Integer id) {
+        logger.warn("Attempting to delete employee with ID: {}", id);
         if (empRepository.existsById(id)) {
             empRepository.deleteById(id);
         } else {
